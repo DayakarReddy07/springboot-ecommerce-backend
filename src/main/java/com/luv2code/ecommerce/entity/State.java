4d76c01d381704ce81f55d75b33fb8c51bee54c3
Dayakarms07 <dayakarr177@gmail.com>
2026-02-13 22:57:42 +0530

    Checkout Forms Drop-downs needed with states and countries updated with data rest api.

---
+++ src/main/java/com/luv2code/ecommerce/entity/State.java
@@ -0,0 +1,19 @@
+package com.luv2code.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "state")
+@Data
+public class State {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private short id;
+    @Column(name = "name")
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+}
