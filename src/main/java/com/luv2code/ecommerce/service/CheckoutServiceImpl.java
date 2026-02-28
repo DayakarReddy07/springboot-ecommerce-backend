2b1f9f8667a6cd7a532cda3db4f7e039737be6ba
Dayakarms07 <dayakarr177@gmail.com>
2026-02-28 21:58:27 +0530

    Payment processing with stripe API, Configuration on Backend, Add customer, Email Receipts implemented

--- src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -1,23 +1,29 @@
 package com.luv2code.ecommerce.service;
 
 import com.luv2code.ecommerce.dao.CustomerRespository;
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.entity.Customer;
 import com.luv2code.ecommerce.entity.Order;
 import com.luv2code.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 
     private CustomerRespository customerRespository;
 
-    public CheckoutServiceImpl(CustomerRespository customerRespository) {
+    public CheckoutServiceImpl(CustomerRespository customerRespository,
+                               @Value("${stripe.key.secret}") String secretKey) {
         this.customerRespository = customerRespository;
+        Stripe.apiKey = secretKey;
     }
 
     @Override
@@ -54,6 +60,20 @@ public class CheckoutServiceImpl implements CheckoutService {
         customerRespository.save(customer);
         return new PurchaseResponse(orderTrackingNumber);
     }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+        Map<String,Object> params = new HashMap<>();
+        params.put("amount",paymentInfo.getAmount());
+        params.put("currency",paymentInfo.getCurrency());
+        params.put("payment_method_types",paymentMethodTypes);
+        params.put("description","Luv2Shop purchase");
+        params.put("receipt_email", paymentInfo.getReceiptEmail());
+        return PaymentIntent.create(params);
+    }
+
 
     private String generateOrderTrackingNumber() {
         // generate a random UUID number(UUID version-4) Universal unitque identifier
